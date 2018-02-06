4e781b8a2cb72b04accce666be799e214ac4fd6f
David Lewis and Mariah Shipley <david@>
2018-02-06 15:21:11 -0800

    Add functionality to convert triple digit words

---
+++ lib/numbers_to_words.rb
@@ -0,0 +1,53 @@
+class Numerical
+  def initialize(digit)
+    @digit = digit
+  end
+
+  def numbers_to_words
+    single_digits = {0 => "zero", 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
+
+    double_digits = {0 => "zero", 2 => "twenty",  3 => "thirty", 4 => "forty", 5 => "fifty", 6 => "sixty", 7 => "seventy", 8 => "eighty", 9 => "ninety"}
+
+    digits_array = @digit.to_s.chars.map(&:to_i)
+
+    reversed_digits_array = digits_array.reverse()
+    binding.pry
+    if (digits_array.length == 1) || (digits_array[1] == 1)
+      single_digits.fetch(@digit)
+
+    elsif digits_array.length == 2
+      first_digit = digits_array[1]
+      second_digit = digits_array[0]
+      first_digit = single_digits.fetch(first_digit)
+      second_digit = double_digits.fetch(second_digit)
+
+      if first_digit == "zero"
+        second_digit
+
+      else
+        second_digit + " " + first_digit
+
+      end
+
+    else digits_array.length == 3
+      first_digit = digits_array[2]
+      second_digit = digits_array[1]
+      third_digit = digits_array[0]
+      first_digit = single_digits.fetch(first_digit)
+      second_digit = double_digits.fetch(second_digit)
+      third_digit = single_digits.fetch(third_digit)
+
+      if first_digit == "zero" && second_digit == "zero"
+        third_digit + " " + "hundred"
+
+      elsif first_digit == "zero"
+      third_digit + " " + "hundred" + " " + second_digit
+
+      else
+        third_digit + " " + "hundred" + " " + second_digit + " " + first_digit
+
+      end
+
+    end
+  end
+end
---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,36 @@
+require('rspec')
+require('numbers_to_words')
+require('pry')
+
+describe('numbers_to_words') do
+  it("return the word version of single digit number") do
+    number2 = Numerical.new(2)
+    expect(number2.numbers_to_words()).to(eq("two"))
+  end
+
+  it("return the word version of any single digit number") do
+    number3 = Numerical.new(0)
+    expect(number3.numbers_to_words()).to(eq("zero"))
+  end
+
+  it("return the word version of any double digit number") do
+    number3 = Numerical.new(34)
+    expect(number3.numbers_to_words()).to(eq("thirty four"))
+  end
+
+  it("return the word version of any double digit number and removing 'zero' when not needed") do
+    number3 = Numerical.new(80)
+    expect(number3.numbers_to_words()).to(eq("eighty"))
+  end
+
+  it("return the word version of any triple digit number and removing 'zero' when not needed") do
+    number4 = Numerical.new(142)
+    expect(number4.numbers_to_words()).to(eq("one hundred forty two"))
+  end
+
+  it("return the word version of any triple digit number and removing 'zero' when not needed") do
+    number3 = Numerical.new(200)
+    expect(number3.numbers_to_words()).to(eq("two hundred"))
+  end
+
+end
